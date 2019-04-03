4d7619bbc60ae451968650be23338b817129c921
Svetlana <florina33@gmail.com>
2019-04-04 00:22:29 +0300

    minor edits

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-
-$('.tooltip').on('click', function (event) {
-    alert('fsdfasd');
-});
-
 $( ".round input[type=radio]" ).on( "click", function() {
 
-    $(".round input[type=radio]").each(function(i){  
-        
+    $( ".round input[type=radio]" ).each(function(i){  
         let parent = $(this).parents(".item-plan");
         
         if( $(this).is(":checked") ) {
@@ -15,10 +9,7 @@ $( ".round input[type=radio]" ).on( "click", function() {
         else {
             parent.removeClass("active");
         }
-
     });
 
     return false;
-
-    // parent.toggleClass("active");
 });
