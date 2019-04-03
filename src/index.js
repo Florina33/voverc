26e197d34c81467c853f42c7116e9398548c09fb
Svetlana <florina33@gmail.com>
2019-04-03 21:33:47 +0300

    rename .wrap-radio

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ $('.tooltip').on('click', function (event) {
     alert('fsdfasd');
 });
 
-
 $( ".round input[type=radio]" ).on( "click", function() {
 
     $(".round input[type=radio]").each(function(i){  
         
-        let parent = $(this).parents(".wrap-checkbox");
+        let parent = $(this).parents(".item-plan");
         
         if( $(this).is(":checked") ) {
             parent.addClass("active");
